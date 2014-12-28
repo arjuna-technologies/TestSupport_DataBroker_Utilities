ed636dbb3b1183d51f5b056fc44bda671fbc6975
Stuart Wheater <stuart.wheater@arjuna.com>
2014-12-28 10:00:02 +0000

    Support lifecycle

---
+++ dataflownodes-lifecycle/src/main/java/com/arjuna/dbutilities/testsupport/dataflownodes/lifecycle/DummyDataFlowNodeState.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2013-2014, Arjuna Technologies Limited, Newcastle-upon-Tyne, England. All rights reserved.
+ */
+
+package com.arjuna.dbutilities.testsupport.dataflownodes.lifecycle;
+
+import java.io.Serializable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import com.arjuna.databroker.data.jee.DataFlowNodeState;
+
+public class DummyDataFlowNodeState implements DataFlowNodeState
+{
+    private static final Logger logger = Logger.getLogger(DummyDataFlowNodeState.class.getName());
+
+    public DummyDataFlowNodeState(String id)
+    {
+        logger.log(Level.FINE, "DummyDataFlowNodeState: " + id);
+
+        _id = id;
+    }
+
+    public String getId()
+    {
+        return _id;
+    }
+
+    @Override
+    public Serializable getState()
+    {
+        return _state;
+    }
+
+    @Override
+    public void setState(Serializable state)
+    {
+        _state = state;
+    }
+
+    private String       _id;
+    private Serializable _state;
+}
