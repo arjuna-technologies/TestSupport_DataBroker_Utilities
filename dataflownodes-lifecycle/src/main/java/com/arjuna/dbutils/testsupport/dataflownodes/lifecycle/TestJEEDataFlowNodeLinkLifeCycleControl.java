d7e0d9971686bd37c3e4a1ebcfa8b6f20478d449
Stuart Wheater <stuart.wheater@arjuna.com>
2015-10-14 10:50:14 +0100

    Initial Link support

---
+++ dataflownodes-lifecycle/src/main/java/com/arjuna/dbutils/testsupport/dataflownodes/lifecycle/TestJEEDataFlowNodeLinkLifeCycleControl.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2014-2015, Arjuna Technologies Limited, Newcastle-upon-Tyne, England. All rights reserved.
+ */
+
+package com.arjuna.dbutils.testsupport.dataflownodes.lifecycle;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import com.arjuna.databroker.data.DataFlow;
+import com.arjuna.databroker.data.DataFlowNode;
+import com.arjuna.databroker.data.core.DataFlowNodeLinkLifeCycleControl;
+import com.arjuna.databroker.data.core.DataFlowNodeLinkManagementException;
+import com.arjuna.databroker.data.core.NoCompatableCommonDataTransportTypeException;
+import com.arjuna.databroker.data.core.NoCompatableCommonDataTypeException;
+
+public class TestJEEDataFlowNodeLinkLifeCycleControl implements DataFlowNodeLinkLifeCycleControl
+{
+    private static final Logger logger = Logger.getLogger(TestJEEDataFlowNodeLinkLifeCycleControl.class.getName());
+
+    @Override
+    public <T> Boolean createDataFlowNodeLink(DataFlowNode sourceDataFlowNode, DataFlowNode sinkDataFlowNode, DataFlow dataFlow)
+        throws DataFlowNodeLinkManagementException, NoCompatableCommonDataTypeException, NoCompatableCommonDataTransportTypeException
+    {
+    	logger.log(Level.WARNING, "TestJEEDataFlowNodeLinkLifeCycleControl.createDataFlowNodeLink");
+
+    	throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public <T> Boolean recreateDataFlowNodeLink(DataFlowNode sourceDataFlowNode, DataFlowNode sinkDataFlowNode, DataFlow dataFlow)
+        throws DataFlowNodeLinkManagementException, NoCompatableCommonDataTypeException, NoCompatableCommonDataTransportTypeException
+    {
+    	logger.log(Level.WARNING, "TestJEEDataFlowNodeLinkLifeCycleControl.recreateDataFlowNodeLink");
+
+    	throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public <T> Boolean removeDataFlowNodeLink(DataFlowNode sourceDataFlowNode, DataFlowNode sinkDataFlowNode, DataFlow dataFlow)
+        throws DataFlowNodeLinkManagementException, NoCompatableCommonDataTypeException, NoCompatableCommonDataTransportTypeException
+    {
+    	logger.log(Level.WARNING, "TestJEEDataFlowNodeLinkLifeCycleControl.removeDataFlowNodeLink");
+
+    	throw new UnsupportedOperationException();
+    }
+}
